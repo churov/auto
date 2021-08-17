3bfd273147fe5e80fc27e5532d8d0fe968edd8ba
Artsiom Churau <churovaa@yandex.by>
2021-08-17 11:35:41 +0300

    Homework12-only_beginning+plugins

--- auto1/src/main/java/Homework12/Triangle.java
+++ auto1/src/main/java/Homework12/Triangle.java
@@ -1,11 +1,11 @@
 package Homework12;
 
 public class Triangle {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         double a,b,c,p,s;
         a = 3;
         b = 4;
-        c = 5;
+        c = 6;
         p = (a+b+c)/2;
         s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
